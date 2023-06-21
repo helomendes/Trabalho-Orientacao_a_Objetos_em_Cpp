4e1fb773a70c7f5ef95e0b3997c82e6ee90e5f8c
helololo <99905001+helomendes@users.noreply.github.com>
2023-06-21 14:08:46 -0300

    Update libJogador.cpp

--- Orientação a Objetos/libJogador.cpp
+++ Orientação a Objetos/libJogador.cpp
@@ -238,15 +238,11 @@ void imprimirJogador(Jogador* j) {
 }
 
 void imprimirJogadores(std::list<Jogador*> lista) {
-	if (lista.size() <= 0) {
-		std::cout << "Não existem jogadores.\n";
-	} else {
-		unsigned short int i = 1;
-		std::list<Jogador*>::iterator it;
-		for (it = lista.begin(); it != lista.end(); it++) {
-			std::cout << '\n' << "\t-----" << i << "-----" << '\n';
-			imprimirJogador(*it);
-			i++;
-		}
+	unsigned short int i = 1;
+	std::list<Jogador*>::iterator it;
+	for (it = lista.begin(); it != lista.end(); it++) {
+		std::cout << '\n' << "\t-----" << i << "-----" << '\n';
+		imprimirJogador(*it);
+		i++;
 	}
 }
